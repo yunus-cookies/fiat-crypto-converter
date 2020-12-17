9ca6506b572ed8da8d7179dc92f0755afcf19dbe
yunus-cookies <yunusokutan@mail.com>
2020-12-17 21:06:32 +0100

    Updates

---
+++ precache-manifest.d42f1abd5bd0063ba253ec5a8e6e3f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ecb221f278ec47b3a7ad41ee128bf7",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "1639bab76c186a2dc21b",
+    "url": "/project1/static/css/main.a94a598b.chunk.css"
+  },
+  {
+    "revision": "cc3c01e8b453f2063a89",
+    "url": "/project1/static/js/2.931840f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project1/static/js/2.931840f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1639bab76c186a2dc21b",
+    "url": "/project1/static/js/main.fda66f53.chunk.js"
+  },
+  {
+    "revision": "f0fcff80a42bac7c0c81",
+    "url": "/project1/static/js/runtime-main.1e0687d9.js"
+  }
+]);
